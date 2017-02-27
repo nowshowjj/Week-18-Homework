46945f774924cb9dd780b4511d83c77d1c6aa59a
edwin rivera <techedwin@aol.com>
2017-02-27 07:58:39 -0500

    Commiting server js file with new content

--- README.md
+++ README.md
@@ -9,3 +9,77 @@ immense-bayou-85375
 https://immense-bayou-85375.herokuapp.com/
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Content I might use later:
+Save Below||||
+------------------------------------
+
+//First Scrapper method in a function
+
+getPosts('https://www.reddit.com/r/webdev/');
+
+function getPosts(url) {
+  count++;
+  request(url, function(err, resp, body){
+    // this passes all of the html to the $.
+    var $ = cheerio.load(body);
+    // from here we can look for specific things
+
+    //for example we are looking for the <p class="title"> and getting the link of the article
+    $('p.title').each(function(idx, element){
+      var title = $(this).text();
+      var link = $(this).children().attr("href");
+
+    // pushing to reults variable
+      result.push({
+        title:title,
+        link:link
+      })
+    });
+    // this is the link for the next button that reveals more results
+    var next = $('span.next-button a').attr('href')
+    
+    // making count go up so it doesnt infinetly give us all the pages from tge begining.
+    if (count === 3) {
+      console.log(result);
+    } else {
+      getPosts(next);
+    }
+  });
+
+}
+
--- server.js
+++ server.js
@@ -1,46 +1,20 @@
+//Requires
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 var cheerio = require('cheerio');
 var request = require('request');
 
-//putting the results in a array
-var result = [];
-var count = 0 ;
+mongoose.Promise = Promise;
 
 
+// Model names
+var News = require("./news.js");
+var Users = require("./users.js");
+
+//Express
+var app = express();
 
 
-getPosts('https://www.reddit.com/r/webdev/');
-
-function getPosts(url) {
-  count++;
-  request(url, function(err, resp, body){
-    // this passes all of the html to the $.
-    var $ = cheerio.load(body);
-    // from here we can look for specific things
-
-    //for example we are looking for the <p class="title"> and getting the link of the article
-    $('p.title').each(function(idx, element){
-      var title = $(this).text();
-      var link = $(this).children().attr("href");
-
-    // pushing to reults variable
-      result.push({
-        title:title,
-        link:link
-      })
-    });
-    // this is the link for the next button that reveals more results
-    var next = $('span.next-button a').attr('href')
-    
-    // making count go up so it doesnt infinetly give us all the pages from tge begining.
-    if (count === 3) {
-      console.log(result);
-    } else {
-      getPosts(next);
-    }
-  });
-
-}
-
-
-
-
+//Body-parser
