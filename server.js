c24095414579e99dffcf1c7f8574959e44468c47
edwin rivera <techedwin@aol.com>
2017-02-26 20:33:51 -0500

    Adding few content

--- server.js
+++ server.js
@@ -1,117 +1,46 @@
-/* Showing Mongoose's "Populated" Method (18.3.8)
- * INSTRUCTOR ONLY
- * =============================================== */
+var cheerio = require('cheerio');
+var request = require('request');
 
-// Dependencies
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-// Requiring our Note and Article models
-var Note = require("./models/Note.js");         // route to module
-var Article = require("./models/Article.js");   // route to module
-// scraping tools
-var request = require("request");
-var cheerio = require("cheerio");
-// Set mongoose to leverage built in JavaScript ES6 Promises
-mongoose.Promise = Promise;
+//putting the results in a array
+var result = [];
+var count = 0 ;
 
 
-// Initialize Express
-var app = express();
-
-// Use morgan and body parser with our app
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-
-// Make public a static dir
-app.use(express.static("public"));
-
-// Database configuration with mongoose
-mongoose.connect("mongodb://localhost/week18day3mongoose");
-var db = mongoose.connection;
-
-// Show any mongoose errors
-db.on("error", function(error) {
-  console.log("Mongoose Error: ", error);
-});
-
-// Once logged in to the db through mongoose, log a success message
-db.once("open", function() {
-  console.log("Mongoose connection successful.");
-});
 
 
-// Routes
-// ======
+getPosts('https://www.reddit.com/r/webdev/');
 
-// A GET request to scrape the echojs website
-app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with request
-  request("http:/      |/)(*&^%$#@!wesite goes here!@#$%^&*()/|       ", function(error, response, html) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(html);
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+function getPosts(url) {
+  count++;
+  request(url, function(err, resp, body){
+    // this passes all of the html to the $.
+    var $ = cheerio.load(body);
+    // from here we can look for specific things
 
-      // Save an empty result object
-      var result = {};
+    //for example we are looking for the <p class="title"> and getting the link of the article
+    $('p.title').each(function(idx, element){
+      var title = $(this).text();
+      var link = $(this).children().attr("href");
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this).children("a").text();
-      result.link = $(this).children("a").attr("href");
-
-      // Using our Article model, create a new entry
-      // This effectively passes the result object to the entry (and the title and link)
-      var entry = new Article(result);
-
-      // Now, save that entry to the db
-      entry.save(function(err, doc) {
-        // Log any errors
-        if (err) {
-          console.log(err);
-        }
-        // Or log the doc
-        else {
-          console.log(doc);
-        }
-      });
-
+    // pushing to reults variable
+      result.push({
+        title:title,
+        link:link
+      })
     });
+    // this is the link for the next button that reveals more results
+    var next = $('span.next-button a').attr('href')
+    
+    // making count go up so it doesnt infinetly give us all the pages from tge begining.
+    if (count === 3) {
+      console.log(result);
+    } else {
+      getPosts(next);
+    }
   });
-  
+
+}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-// Tell the browser that we finished scraping the text
-  res.send("Scrape Complete");
-});
-
-// This will get the articles we scraped from the mongoDB
-app.get("/articles", function(req, res) {
-});
-
-// Grab an article by it's ObjectId
-app.get("/articles/:id", function(req, res) {
-});
-
-// Create a new note or replace an existing note
-app.post("/articles/:id", function(req, res) {
-});
-
-
-// Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
-});
