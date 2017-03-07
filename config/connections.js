d001d74d237ecdbc8c5154f87106b1a18e20c445
edwin rivera <techedwin@aol.com>
2017-03-06 20:31:20 -0500

    bug fix heroku

--- config/connections.js
+++ config/connections.js
@@ -6,7 +6,7 @@ var Promise = require('bluebird');
 mongoose.Promise = Promise;
 
 //db configuration
-mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds119380.mlab.com:19380/heroku_8n8jg9gb");
+mongoose.connect("mongodb://123:123@ds119380.mlab.com:19380/heroku_8n8jg9gb");
 var db = mongoose.connection;
 
 //mongoose errors
